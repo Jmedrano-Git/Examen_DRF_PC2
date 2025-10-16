f9bcf6f5cb87cea9c9981fbd49f867b9fe84534a
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-15 22:56:37 -0500

    Creation endpoints and requeriments minium

--- games/views.py
+++ games/views.py
@@ -3,3 +3,15 @@ from rest_framework.filters import SearchFilter
 from .models import GameDb, CompanyDb
 from .serializers import GameListSerializer, CompanySerializer
 
+class GameViewSet(viewsets.ModelViewSet):
+    # Los endpoints(Get,Post,el GET/id o el PUT/id, el DELETE/id o buscar los datos que ponga por su titulo o el genero)
+    # Usando ?search=juego1xd como ejemplo
+    queryset = GameDb.objects.select_related('company').all()
+    serializer_class = GameListSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'genre'] #Aquí permite el ?search=juego1xd
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    # Lo mismo todos los endpoints pero aquí ya muestra los juegos que pertenecen a cada compañía 
+    queryset = CompanyDb.objects.prefetch_related('games').all()
+    serializer_class = CompanySerializer
