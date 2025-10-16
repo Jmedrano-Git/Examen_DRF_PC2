8483854e36664b8374de5f5851df7d1279a7f11c
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-15 22:47:49 -0500

    Imports viewsets, models and serialis in views.py

--- games/views.py
+++ games/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from .models import GameDb, CompanyDb
+from .serializers import GameListSerializer, CompanySerializer
 
-# Create your views here.
