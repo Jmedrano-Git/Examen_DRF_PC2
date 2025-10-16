d799e36f80b2512bc1b3adf7bc8c529829430d6f
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-15 23:00:47 -0500

    Add routers

--- games/urls.py
+++ games/urls.py
@@ -2,3 +2,10 @@ from rest_framework.routers import DefaultRouter
 from .views import GameViewSet, CompanyViewSet
 from django.urls import path, include
 
+router = DefaultRouter()
+router.register(r'games', GameViewSet, basename='games')
+router.register(r'companies', CompanyViewSet, basename='companies')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
