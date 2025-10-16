6a1afebb7c4c2528679d07da6005f8d6f8dc3728
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-15 23:17:25 -0500

    Correction in models

--- games/models.py
+++ games/models.py
@@ -5,8 +5,8 @@ class CompanyDb(models.Model):
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=100, blank=True)
 
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['name']
