24a3005cfe5dd76343ca77093b2a0bb85a802cf9
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-15 22:34:24 -0500

    Add field to point +1

--- games/serializers.py
+++ games/serializers.py
@@ -1,2 +1,6 @@
 from rest_framework import serializers
 from .models import CompanyDb, GameDb
+
+class GameListSerializer(serializers.ModelSerializer):
+    #El campo de solo lectura para mostrar el nombre de la compañía (Punto extra)
+    company_name = serializers.CharField(source='company.name', read_only=True)
