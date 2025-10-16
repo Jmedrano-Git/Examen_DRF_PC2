b7e7c0193ea32b6fe0b3eb1349cd35ece5fc1506
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-15 22:27:05 -0500

    Add models the company and game, and relationship

--- games/models.py
+++ games/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100, blank=True)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'companies'
+
+    def __str__(self):
+        return self.name
+    
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    release_year = models.IntegerField(null=True, blank=True)
+    company = models.ForeignKey(Company, related_name='games', on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    
+    def __str__(self):
+        return self.title
